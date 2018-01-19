e993ced3d0a90b982d40a7add07cfebd931c6cf3
Jeff Boothe <jeff.boothe@reurgency.com>
2018-01-19 01:07:35 -0500

    feat(Lookups): add getter and setters

--- src/app/common/entities/CaseType.ts
+++ src/app/common/entities/CaseType.ts
@@ -3,11 +3,28 @@ import { CasePhase } from "./CasePhase";
 
 export class CaseType {
 
-  caseTypeOID: number;
+  private _id: number;
+  private _caseTypeOID: number;
   name: string;
   shortName: string;
   description: string;
   casePhases: CasePhase[];
   courtOID: number;
+
+  public get id():number {
+    return  this._id;
+  }
+  public set id(value:number){
+      this._id = value;
+      this._caseTypeOID = value;
+  }
+
+  public get caseTypeOID():number {
+    return  this._caseTypeOID;
+  }
+  public set caseTypeOID(value:number){
+      this._caseTypeOID = value;
+      this._id = value;
+  }
 
 }
--- src/app/common/entities/HearingType.ts
+++ src/app/common/entities/HearingType.ts
@@ -1,11 +1,47 @@
 
 export class HearingType {
 
-  hearingTypeOID: number;
-  hearingName: string;
+  private _id: number;
+  private _hearingTypeOID: number;
+  private _name: string;
+  private _hearingName: string;
   durationInMinutes: number;
   courtOID: number;
   description: string;
+
+
+
+  public get id():number {
+      return  this._id;
+  }
+  public set id(value:number){
+      this._id = value;
+      this._hearingTypeOID = value;
+  }
+
+  public get hearingTypeOID():number{
+    return  this._hearingTypeOID;
+  }
+  public set hearingTypeOID(value:number){
+      this._hearingTypeOID = value;
+      this._id = value;
+  }
+
+  public get name():string{
+      return  this._name;
+  }
+  public set name(value:string){
+      this._name = value;
+      this._hearingName = value;
+  }
+
+  public get hearingName():string{
+    return  this._hearingName;
+  }
+  public set hearingName(value:string){
+      this._hearingName = value;
+      this._name = value;
+  }
 
 
 }
