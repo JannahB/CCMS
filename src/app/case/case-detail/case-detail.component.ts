5176d0d6df50f576e190d7db4f96d28145b57782
Joe Newton <joe.newton@reurgency.com>
2018-01-22 18:05:34 -0500

    feat(Case Events): fix saving of case events

--- src/app/case/case-detail/case-detail.component.ts
+++ src/app/case/case-detail/case-detail.component.ts
@@ -30,6 +30,9 @@ import { LookupService } from '../../common/services/http/lookup.service';
 import { CaseTaskDTO } from '../../common/entities/CaseTaskDTO';
 import { JudicialAssignment } from '../../common/entities/JudicialAssignment';
 import { JudicialOfficer } from '../../common/entities/JudicialOfficer';
+import { CaseType } from '../../common/entities/CaseType';
+import { CaseStatus } from '../../common/entities/CaseStatus';
+import { CasePhase } from '../../common/entities/CasePhase';
 
 
 @Component({
@@ -54,6 +57,10 @@ export class CaseDetailComponent implements OnInit, OnDestroy {
   documentTemplateTypes: DocTemplate[] = [];
   selectedDocumentTemplateType: DocTemplate;
   routeSubscription: Subscription;
+  caseTypes:CaseType[] = [];
+  caseStatuses:CaseStatus[] = [];
+  casePhases:CasePhase[] = [];
+
 
   datePipe:DatePipe = new DatePipe("en");
 
@@ -81,6 +88,15 @@ export class CaseDetailComponent implements OnInit, OnDestroy {
     this.caseSvc
       .fetchDocumentTemplate()
       .subscribe(results => this.documentTemplateTypes = results);
+
+    this.caseSvc
+      .fetchCaseType()
+      .subscribe(results => this.caseTypes = results);
+      
+    this.caseSvc
+      .fetchCaseStatus()
+      .subscribe(results => this.caseStatuses = results);
+    
   }
 
   ngOnDestroy() {
@@ -102,6 +118,12 @@ export class CaseDetailComponent implements OnInit, OnDestroy {
         this.toastSvc.showWarnMessage('There is no case with caseOID of '+ caseId +'.', 'No Case Found');
       } else {
         this.case = kase;
+
+        if(this.case.caseType){
+          this.caseSvc
+            .fetchPhaseByType(this.case.caseType.caseTypeOID)
+            .subscribe(results => this.casePhases = results);
+        }
       }
     });
   }
@@ -135,6 +157,15 @@ export class CaseDetailComponent implements OnInit, OnDestroy {
 
   }
 
+  caseTypeChange(event):void{
+    if(this.case.caseType){
+      this.caseSvc
+        .fetchPhaseByType(this.case.caseType.caseTypeOID)
+        .subscribe(results => this.casePhases = results);
+    }else{
+      this.casePhases = [];
+    }
+  }
 
 
   // MODALS --------------------------------------------
@@ -600,6 +631,14 @@ export class CaseDetailComponent implements OnInit, OnDestroy {
   documents: any[];
   selectedInitiatedByParty: CaseParty;
 
+  caseEventOnRowSelect(event):void{
+    this.caseEvent = event.data;
+  }
+
+  requestViewCaseEvent(){
+    this.showEventModal();
+  }
+
   addCaseEventClicked(){
     this.caseEvent = new CaseEvent();
 
@@ -630,6 +669,8 @@ export class CaseDetailComponent implements OnInit, OnDestroy {
     this.caseSvc
       .saveCaseEvent(this.caseEvent)
       .subscribe(savedCaseEvent => {
+        savedCaseEvent.eventType = this.eventTypes.find(e => e.eventTypeOID == savedCaseEvent.eventType.eventTypeOID);
+
         let index:number = this.case.caseEvents
           .findIndex(ce => ce.caseEventOID == savedCaseEvent.caseEventOID);
 
@@ -640,6 +681,8 @@ export class CaseDetailComponent implements OnInit, OnDestroy {
           this.case.caseEvents = this.case.caseEvents.slice();
         }
       });
+
+    this.showModalAddEvent = false;
   }
 
   saveCase(shouldShowSuccessMessage:boolean = true){
