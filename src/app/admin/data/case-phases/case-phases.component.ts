d8ad6048e718473b5cfb9618f8f759e36adc9a7b
Jeff Boothe <jeff.boothe@reurgency.com>
2018-02-22 01:28:04 -0500

    feat(Case Phase): add base component

---
+++ src/app/admin/data/case-phases/case-phases.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-case-phases',
+  templateUrl: './case-phases.component.html',
+  styleUrls: ['./case-phases.component.scss']
+})
+export class CasePhasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
