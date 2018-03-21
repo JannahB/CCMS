1feeb849318e5305105e8694b9a052566a3cc207
jboothe <jeff.boothe@reurgency.com>
2018-03-21 17:09:04 -0400

    feat(app): gracefully handle 403 & 500 errors

--- src/app/common/interceptors/authorization.interceptor.ts
+++ src/app/common/interceptors/authorization.interceptor.ts
@@ -1,3 +1,4 @@
+import { ToastService } from './../services/utility/toast.service';
 import { Injectable } from '@angular/core';
 import { RequestOptions, RequestOptionsArgs, Headers } from '@angular/http';
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
@@ -11,62 +12,85 @@ import { AuthenticationModel } from '../model/authentication-model';
 @Injectable()
 export class AuthorizationInterceptor implements HttpInterceptor {
 
-    private static readonly AUTH_TOKEN:string = "AUTH_TOKEN";
-    private static staticLocalStorageService:LocalStorageService = null;
+  private static readonly AUTH_TOKEN: string = "AUTH_TOKEN";
+  private static staticLocalStorageService: LocalStorageService = null;
 
-    private static _authToken:string = null;
-    public static get authToken():string{ return AuthorizationInterceptor._authToken; }
-    public static set authToken(value:string){
-        AuthorizationInterceptor._authToken = value;
+  private static _authToken: string = null;
+  public static get authToken(): string { return AuthorizationInterceptor._authToken; }
+  public static set authToken(value: string) {
+    AuthorizationInterceptor._authToken = value;
 
-        AuthorizationInterceptor.staticLocalStorageService
-            .setValue(AuthorizationInterceptor.AUTH_TOKEN, value);
+    AuthorizationInterceptor.staticLocalStorageService
+      .setValue(AuthorizationInterceptor.AUTH_TOKEN, value);
+  }
+
+  constructor(
+    private localStorageService: LocalStorageService,
+    private router: Router,
+    private authenticationModel: AuthenticationModel,
+    private toastSvc: ToastService
+  ) {
+
+    AuthorizationInterceptor.staticLocalStorageService = localStorageService;
+
+    if (localStorageService.hasValue(AuthorizationInterceptor.AUTH_TOKEN)) {
+      AuthorizationInterceptor.authToken = localStorageService.getValue(AuthorizationInterceptor.AUTH_TOKEN);
+    }
+  }
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+
+    if (!AuthorizationInterceptor.authToken) {
+      return next.handle(req);
     }
 
-    constructor(
-        private localStorageService:LocalStorageService,
-        private router:Router,
-        private authenticationModel:AuthenticationModel
-    ){
+    let request = req.clone({
+      setHeaders: {
+        token: AuthorizationInterceptor.authToken,
+        Authorization: `Bearer ${AuthorizationInterceptor.authToken}`
+      }
+    });
 
-        AuthorizationInterceptor.staticLocalStorageService = localStorageService;
+    return next.handle(request)
+      .do(
+        event => event,
+        error => {
 
-        if(localStorageService.hasValue(AuthorizationInterceptor.AUTH_TOKEN)){
-            AuthorizationInterceptor.authToken = localStorageService.getValue(AuthorizationInterceptor.AUTH_TOKEN);
-        }
-    }
+          // This is difficult to test.  Just navigate to login on all errors for now
+          // Until the server sends a 401 status code, leave this commented
+          // Otherwise it hangs when refreshing a page when token is expired
+          if (error instanceof HttpErrorResponse) {
 
-    intercept(
-        req: HttpRequest<any>,
-        next: HttpHandler
-    ): Observable<HttpEvent<any>> {
+            if (error.status == 401) {
+              this.authenticationModel.returnUrl = this.router.url;
+              AuthorizationInterceptor.authToken = null;
+              this.router.navigate(["login"]);
 
-        if(!AuthorizationInterceptor.authToken){
-            return next.handle(req);
-        }
+            } else if (error.status == 403) {
+              this.toastSvc.showWarnMessage(
+                'Your role does not allow access to the requested feature.',
+                'Forbidden'
+              );
 
-        let request = req.clone({
-            setHeaders: {
-                token: AuthorizationInterceptor.authToken,
-                Authorization: `Bearer ${AuthorizationInterceptor.authToken}`
+            } else if (error.status == 500) {
+              this.toastSvc.showErrorMessage(
+                'Please accept our apologies! An error occurred. Please report the steps that can be taken to reproduce this to your system administrator.',
+                'Server Error 500'
+              );
             }
-        });
 
-        return next.handle(request)
-            .do(
-                event => event,
-                error => {
-                    this.authenticationModel.returnUrl = this.router.url;
-                    AuthorizationInterceptor.authToken = null;
-
-                    // This is difficult to test.  Just navigate to login on all errors for now
-                    // Until the server sends a 401 status code, leave this commented
-                    // Otherwise it hangs when refreshing a page when token is expired
-                    // if(error instanceof HttpErrorResponse && error.status == 401){
-                    //     this.router.navigate(["login"]);
-                    // }
-                    this.router.navigate(["login"]);
-                }
-            );
-    }
+            else {
+              // anything else here
+              this.toastSvc.showWarnMessage(
+                'An error occurred.',
+                error.status + ' Error'
+              );
+            }
+          }
+        }
+      );
+  }
 }
