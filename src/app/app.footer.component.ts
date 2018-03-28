ce447233b01375db92807b2947066ea5df12b8c4
jboothe <jeff.boothe@reurgency.com>
2018-03-28 11:40:08 -0400

    feat(app): add version to footer

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -1,19 +1,32 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
-    selector: 'app-footer',
-    template: `
-        <div class="footer">
-            <div class="card clearfix">
-                <span class="footer-text-left">CCMS T&amp;T</span>
-                <span class="footer-text-right">
-                    <span class="material-icons ui-icon-copyright"></span>
-                    <span>All Rights Reserved</span>
-                </span>
-            </div>
-        </div>
+  selector: 'app-footer',
+  styles: [
+    `
+    .footer { font-size: 12px; }
+    .footer .footer-text-left { margin-right: 5px; }
+    .footer .ui-icon-copyright { margin: 3px; }
+    `
+  ],
+  template: `
+    <div class="footer">
+      <div class="card clearfix">
+        <span class="">
+          <span class="material-icons ui-icon-copyright footer-text-left"></span>
+          <span class="footer-text-left"> Copyright 2018</span>
+          <span class="footer-text-left"> CCMS T&amp;T</span>
+        </span>
+        <span class="footer-text-right">
+          ui: v{{version}}
+        </span>
+      </div>
+    </div>
     `
 })
 export class AppFooterComponent {
+
+  public version: string = environment.VERSION;
 
 }
