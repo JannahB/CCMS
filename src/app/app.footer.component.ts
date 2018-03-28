c7f666c95e874b7688d4abecb6db4801264e1f5d
jboothe <jeff.boothe@reurgency.com>
2018-03-28 14:36:37 -0400

    style(app): fixed footer on desktop; footer full width;

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -8,11 +8,21 @@ import { environment } from '../environments/environment';
     .footer { font-size: 12px; }
     .footer .footer-text-left { margin-right: 5px; }
     .footer .ui-icon-copyright { margin: 3px; }
+    @media (min-width: 1025px) {
+      .footer {
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        margin-bottom: 0;
+        padding-bottom: 0 !important;
+      }
+    }
     `
   ],
   template: `
     <div class="footer">
-      <div class="card clearfix">
+      <div class="card clearfix mb-0">
         <span class="">
           <span class="material-icons ui-icon-copyright footer-text-left"></span>
           <span class="footer-text-left"> Copyright 2018</span>
