409b50f7a44d7d3c316d9bc4b25499b35ff08ecb
jboothe <jeff.boothe@reurgency.com>
2018-07-18 14:38:02 -0400

    feat(*): add crim-test ENV

---
+++ src/environments/environment.crim-test.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `angular-cli.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: "https://129.150.194.137/CCMS3",  // TEST CRIMINAL 129.150.194.137 testcrim.ttlawcourts.org
+  mockUrl: "assets/mockData/",
+
+  allowDeleteLookupItems: false,
+  allowResetPasswordFeature: true,
+  allowAdminWorkflowFeature: true,
+
+
+  VERSION: require('../../package.json').version
+};
