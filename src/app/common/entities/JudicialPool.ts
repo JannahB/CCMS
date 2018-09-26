3026aa5b72ed70355ed3826945e0a9a656ac6c52
Juan Calderin <jualin289@gmail.com>
2018-09-25 22:03:02 -0400

    feat(assignmentManager): adding entities

---
+++ src/app/common/entities/JudicialPool.ts
@@ -0,0 +1,8 @@
+export class JudicialPool {
+  id: number = null;
+  weightAllocation: number = null;
+  caseCount: number = null;
+  active: boolean;
+  courtroomStaff: number = null;
+  allocationPeriod: number = null;
+}
