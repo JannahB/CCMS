39e439029fa5fd33da9e5b220a9b4287901dd572
Jeff Boothe <jeff.boothe@reurgency.com>
2018-10-17 16:49:02 -0400

    chore(*): correct env variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,10 +9,10 @@ export const environment = {
   mockUrl: '../assets/mockData',
 
   allowDeleteLookupItems: false,
-  allowResetPasswordFeature: false,
-  allowAdminWorkflowFeature: false,
-  allowAdminCalendarFeature: false,
-  allowJudgeAssignMgmtFeature: false,
+  allowResetPasswordFeature: true,
+  allowAdminWorkflowFeature: true,
+  allowAdminCalendarFeature: true,
+  allowJudgeAssignMgmtFeature: true,
 
   VERSION: require('../../package.json').version
 };
