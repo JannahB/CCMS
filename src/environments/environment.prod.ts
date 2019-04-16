f7b1bcb828c8ebb818087b0046731bfbd0f23bd6
Jeff Boothe <jeff.boothe@reurgency.com>
2019-04-16 15:06:49 -0400

    chore(dev): tt-dev correct apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: "https://cms.ttlawcourts.org/CCMS3",       // PROD
+  // apiUrl: "https://childcms.ttlawcourts.org/CCMS3",       // PROD
+  // apiUrl: "http://tt-qa.eastus2.cloudapp.azure.com/CCMS3",       // QA
+  apiUrl: "http://tt-dev.eastus2.cloudapp.azure.com/CCMS3",       // DEV
   mockUrl: "assets/mockData/",
 
   allowDeleteLookupItems: false,
