ab72b1ce05ac732bb36b5f9be82a7b24c5523c1c
juanpablocalderin <juan.p.calderin@gmail.com>
2021-02-11 20:34:06 -0500

    feat(sentencing): adding services

---
+++ src/app/common/services/http/criminal-charges-modification.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { HttpBaseService } from './http-base.service';
+
+
+@Injectable()
+export class CriminalChargeModificationService extends HttpBaseService<any> {
+
+  // Override Base URL's set in Super
+  protected getBaseUrl(): string {
+    return `${super.getBaseUrl()}/api/criminal-charge-modifications`;
+  }
+}
