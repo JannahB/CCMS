28a0cf6ac7f93ab69a3214cd9a6b1d6fa3c0f7d6
JUDICIARYTT\dwillock <dwillock@ttlawcourts.org>
2021-06-24 23:01:41 -0400

    feat(Payments): Add class to model payment frequency

---
+++ src/app/common/entities/PaymentFrequency.ts
@@ -0,0 +1,29 @@
+export class PaymentFrequency {
+
+  private _id: number;
+  private _paymentFrequencyOID: number;
+
+  name: string;
+  shortName: string;
+  description: string;
+  courtOID: number;
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public set id(value: number){
+      this._id = value;
+      this._paymentFrequencyOID = value;
+  }
+
+  public get paymentFrequencyOID(): number {
+    return this._paymentFrequencyOID;
+  }
+
+  public set paymentFrequencyOID(value: number){
+      this._paymentFrequencyOID = value;
+      this._id = value;
+  }
+
+}
