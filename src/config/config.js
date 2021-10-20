9b82341f3478b7fe63ae5e68e6d9c31caa098865
Rhea Seegobin <r.seegobin@ttlawcourts.org>
2021-10-20 09:57:38 -0400

    Fix(edits to config.js file)

--- src/config/config.js
+++ src/config/config.js
@@ -3,14 +3,14 @@ const config = {
   // It will be populated during VSTS release process
   // so that environment variables can be set during release
 
-  production: false,
+  //production: false,
   apiUrl: "https://criminal-dev-oj.ttlawcourts.org/CCMS3",
   //apiUrl: "http://localhost:8080",  
-  mockUrl: "assets/mockData/",
-  allowDeleteLookupItems: false,
-  allowAdminWorkflowFeature: true,
-  allowResetPasswordFeature: true,
-  allowAdminCalendarFeature: false,
-  VERSION: "0.0.0",
+  // mockUrl: "assets/mockData/",
+  // allowDeleteLookupItems: false,
+  // allowAdminWorkflowFeature: true,
+  // allowResetPasswordFeature: true,
+  // allowAdminCalendarFeature: false,
+  // VERSION: "0.0.0",
   trafficApiUrl: "https://trafapi.ttlawcourts.org:10443"
 };
